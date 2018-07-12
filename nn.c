db44883e72d3c61c537e6af7884ee9959b0be37f
manikanta1996 <41147491+manikanta1996@users.noreply.github.com>
2018-07-12 03:44:45 -0700

    Add files via upload

---
+++ nn.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main() {
+    int n,i,j,a[100],b[100],k,f=0,p=0; 
+    scanf("%d", &n);
+    for(i = 0; i < n; i++){
+	
+       scanf("%d",&a[i]);
+       b[i] = -1;
+}
+        
+        for(i=0 ; i< n ;i++)
+        {
+            p = 1;
+            for(j= i+1 ; j<n ;j++)
+            {
+                if(a[i] == a[j])
+                {
+				p++;
+                b[j] = 0;
+            }
+            
+        }
+        if(b[i] != 0)
+        {
+        	b[i]=p;
+		}
+	}
+    for(i=0 ; i<n ; i++)
+    {
+        if(b[i] == 1)
+        {
+		
+            printf("%d",a[i]);
+    }
+}
+}
