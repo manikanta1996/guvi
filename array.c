6abfa6090cc8a5099c8fc46c1f090f13a5df806e
manikanta1996 <41147491+manikanta1996@users.noreply.github.com>
2018-07-12 04:18:49 -0700

    Add files via upload

---
+++ array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n,i,a[100],sum=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+a[i];
+	}
+	printf("%d",sum);
+	return 0;
+}
